8be2341aa94295664247badf10238a576cb42b59
Harshil Agrawal (harshil1712) <github@harshil.dev>
2022-08-17 14:01:41 +0200

    ✨Update init script

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,21 +1,68 @@
-require('dotenv').config()
 const spaceImport = require('contentful-import')
+const inquirer = require('inquirer');
+const fs = require("fs")
+const path = require("path")
 
-const { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN } = process.env
+const questions = [
+  {
+    type: 'input',
+    name: 'spaceId',
+    message: 'Enter you Contentful Space ID',
+    validate(value) {
+      if(!value.length >0) {
+        return 'Must enter a Space ID'
+      }
+      return true
+    }
+  },
+  {
+    type: 'input',
+    name: 'contentDeliveryApiKey',
+    message: 'Enter you Content Delivery API Key',
+    validate(value) {
+      if(!value.length >0) {
+        return 'Must enter Content Delivery API Key'
+      }
+      return true
+    }
+  },
+  {
+    type: 'input',
+    name: 'contentManagementToken',
+    message: 'Enter you Content Management Token',
+    validate(value) {
+      if(!value.length >0) {
+        return 'Must enter Content Management Token'
+      }
+      return true
+    }
+  }
+]
 
-if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_MANAGEMENT_TOKEN) {
-  throw new Error(
-    [
-      'Parameters missing...',
-      'Please ensure your .env file exists and contains the variables CONTENTFUL_SPACE_ID and CONTENTFUL_MANAGEMENT_TOKEN',
-    ].join('\n')
-  )
-}
-
-spaceImport({
-  contentFile: './contentful/export.json',
-  spaceId: CONTENTFUL_SPACE_ID,
-  managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
+inquirer.prompt(questions)
+.then((answers) =>{
+  const {spaceId, contentDeliveryApiKey, contentManagementToken} = answers
+  console.log('Importing the content model into your Space')
+  spaceImport({
+    contentFile: './contentful/export.json',
+    spaceId: spaceId,
+    managementToken: contentManagementToken
+  })
+  .then(() => console.log('The content model is set up!'))
+  .catch(e=> console.log(e))
+  console.log('Creating .env file...')
+  const file = path.join(__dirname, "..", ".env")
+  const fileContents = [
+    `# All environment variables will be sourced`,
+    `# and made available.`,
+    `# Do NOT commit this file to source control`,
+    `CONTENTFUL_SPACE_ID='${spaceId}'`,
+    `CONTENTFUL_ACCESS_TOKEN='${contentDeliveryApiKey}'`,
+  ]
+  .filter(Boolean)
+  .join('\n')
+  fs.writeFileSync(file,fileContents, "utf-8")
+  console.log(".env file created")
+  console.log(answers)
 })
-  .then(() => console.log('The content model of your space is set up!'))
-  .catch((e) => console.error(e))
+.catch(e => console.log(e))
